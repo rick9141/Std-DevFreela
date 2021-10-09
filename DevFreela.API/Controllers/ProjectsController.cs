f84bb9bfcf53de19519174b418591a8f2784840a
Luis Henrique Gavirate <luis.gavirate@take.net>
2021-10-09 04:03:45 -0300

    Add project files.

---
+++ DevFreela.API/Controllers/ProjectsController.cs
@@ -0,0 +1,93 @@
+﻿using DevFreela.API.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DevFreela.API.Controllers
+{
+    [Route("api/projects")]
+    public class ProjectsController : ControllerBase
+    {
+        // api/projects?query=freela
+        [HttpGet]
+        public IActionResult Get(string query)
+        {
+            //search all or filter
+
+            return Ok();
+        }
+
+        // api/projects/5
+        [HttpGet("{id}")]
+        public IActionResult GetById(int id)
+        {
+            //search project
+
+            //return NoteFound();
+
+            return Ok();
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody] CreateProjectModel createProject)
+        {
+            if (createProject.Title.Length > 50)
+            {
+                return BadRequest();
+            }
+
+            //register project
+
+            return CreatedAtAction(nameof(GetById), new { id = createProject.Id }, createProject);
+        }
+
+        //api/projects/91
+        [HttpPut("{id}")]
+        public IActionResult Put(int id, [FromBody] UpdateProjectModel updateProject)
+        {
+            if (updateProject.Description.Length > 200)
+            {
+                return BadRequest();
+            }
+
+            //update project
+
+            return NoContent();
+        }
+
+        // api/projects/91
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            //search, does not exist return NotFound
+
+            // remove
+
+            return NoContent();
+        }
+
+        // api/projects/911/comments
+        [HttpPost("{id}/comments")]
+        public IActionResult PostComment(int id, [FromBody] CreateCommentModel createComment)
+        {
+            return NoContent();
+        }
+
+        // api/projects/5/start
+        [HttpPut("{id}/start")]
+        public IActionResult Start(int id)
+        {
+            return NoContent();
+        }
+
+        // api/projects/1/finish
+        [HttpPut("{id}/finish")]
+        public IActionResult Finish(int id)
+        {
+            return NoContent();
+        }
+
+    }
+}
