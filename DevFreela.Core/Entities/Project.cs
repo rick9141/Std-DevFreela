a7e2d68f678d7c2c19799b02b9703f6d86c1349b
Luis Henrique Gavirate <luis.gavirate@take.net>
2021-10-30 03:11:34 -0300

    feat: add orm and configuring

--- DevFreela.Core/Entities/Project.cs
+++ DevFreela.Core/Entities/Project.cs
@@ -6,11 +6,11 @@ namespace DevFreela.Core.Entities
 {
     public class Project : BaseEntity
     {
-        public Project(string title, string description, int idCliente, int idFreelancer, decimal totalCost)
+        public Project(string title, string description, int idClient, int idFreelancer, decimal totalCost)
         {
             Title = title;
             Description = description;
-            IdCliente = idCliente;
+            IdClient = idClient;
             IdFreelancer = idFreelancer;
             TotalCost = totalCost;
 
@@ -21,8 +21,10 @@ namespace DevFreela.Core.Entities
 
         public string Title { get; private set; }
         public string Description { get; private set; }
-        public int IdCliente { get; private set; }
+        public int IdClient { get; private set; }
+        public User Client { get; private set; }
         public int IdFreelancer { get; private set; }
+        public User Freelancer { get; private set; }
         public decimal TotalCost { get; private set; }
         public DateTime CreatedAt { get; private set; }
         public DateTime? StartedAt { get; private set; }
