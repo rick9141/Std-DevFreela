88f45a2ae144eba26704008ec34f62b4bd84e62d
Luis Henrique <gavirate.henrique@gmail.com>
2022-07-04 01:37:36 -0300

    refactor: Adjusting get all projects with repository pattern

--- DevFreela.Application/Queries/GetAllProjects/GetAllProjectsQueryHandler.cs
+++ DevFreela.Application/Queries/GetAllProjects/GetAllProjectsQueryHandler.cs
@@ -1,7 +1,6 @@
 ﻿using DevFreela.Application.ViewModels;
-using DevFreela.Infrastructure.Persistence;
+using DevFreela.Core.Repositories;
 using MediatR;
-using Microsoft.EntityFrameworkCore;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
@@ -11,19 +10,19 @@ namespace DevFreela.Application.Queries.GetAllProjects
 {
     public class GetAllProjectsQueryHandler : IRequestHandler<GetAllProjectsQuery, List<ProjectViewModel>>
     {
-        private readonly DevFreelaDbContext _dbContext;
-        public GetAllProjectsQueryHandler(DevFreelaDbContext dbContext)
+        private readonly IProjectRepository _projectRepository;
+        public GetAllProjectsQueryHandler(IProjectRepository projectRepository)
         {
-            _dbContext = dbContext;
+            _projectRepository = projectRepository;
         }
 
         public async Task<List<ProjectViewModel>> Handle(GetAllProjectsQuery request, CancellationToken cancellationToken)
         {
-            var projects = _dbContext.Projects;
+            var projects = await _projectRepository.GetAll();
 
-            var projectsViewModel = await projects
+            var projectsViewModel = projects
                 .Select(p => new ProjectViewModel(p.Id, p.Title, p.CreatedAt))
-                .ToListAsync();
+                .ToList();
 
             return projectsViewModel;
         }
--- DevFreela.Core/Repositories/IProjectRepository.cs
+++ DevFreela.Core/Repositories/IProjectRepository.cs
@@ -1,6 +1,11 @@
-﻿namespace DevFreela.Core.Repositories
+﻿using DevFreela.Core.Entities;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace DevFreela.Core.Repositories
 {
-    public interface IPRojectRepository
+    public interface IProjectRepository
     {
+        Task<List<Project>> GetAll();
     }
 }
