51215350aa3f640485c8fbcd62fc37912c2ccda2
Luis Henrique <gavirate.henrique@gmail.com>
2022-07-11 22:47:19 -0300

    feat: creating filter for validation

--- DevFreela.Application/Validators/CreateProjectCommandValidator.cs
+++ DevFreela.Application/Validators/CreateProjectCommandValidator.cs
@@ -9,15 +9,10 @@ namespace DevFreela.Application.Validators
         {
             RuleFor(p => p.Description)
                 .MaximumLength(400)
-                .NotEmpty()
-                .NotNull()
                 .WithMessage("Descrição do projeto pode conter até 400 caracteres.");
 
             RuleFor(p => p.Title)
                 .MaximumLength(55)
-                .MinimumLength(5)
-                .NotEmpty()
-                .NotNull()
                 .WithMessage("Tamanho do Título tem que ter o minimo de 5 e no máximo 55 caracteres.");
         }
     }
