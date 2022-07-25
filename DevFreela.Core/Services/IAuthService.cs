ca4873a01b1442a6cf8dfeb2c1d99dbb8d273fc2
Luis Henrique <gavirate.henrique@gmail.com>
2022-07-25 18:36:49 -0300

    feat: add cryptography password using sha256

--- DevFreela.Core/Services/IAuthService.cs
+++ DevFreela.Core/Services/IAuthService.cs
@@ -1,11 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace DevFreela.Core.Services
+﻿namespace DevFreela.Core.Services
 {
     public interface IAuthService
     {
         string GenerateJwtToken(string email, string role);
+        string ComputeSha256Hash(string password);
     }
 }
