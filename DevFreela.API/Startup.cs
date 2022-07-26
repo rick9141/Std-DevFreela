73c6d5c13fab2463d31983b9bb64870012d56faa
Luis Henrique <gavirate.henrique@gmail.com>
2022-07-25 22:09:12 -0300

    fix: adjust startup app config  token

--- DevFreela.API/Startup.cs
+++ DevFreela.API/Startup.cs
@@ -111,8 +111,9 @@ namespace DevFreela.API
 
             app.UseRouting();
 
-            app.UseAuthorization();
             app.UseAuthentication();
+            app.UseAuthorization();
+
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
